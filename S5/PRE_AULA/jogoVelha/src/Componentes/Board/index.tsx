acaa5019f857ec26a187b04c66b60149e114fec5
luiiizz <luizhenriquebispo05@gmail.com>
2023-03-24 16:50:07 -0300

    v1.0.1 Add jogo da velha

---
+++ S5/PRE_AULA/jogoVelha/src/Componentes/Board/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import '../../assets/css/style.css';
+
+const Board = () => {
+
+    return <div className="board">
+    <div className="cell"></div>
+    <div className="cell"></div>
+    <div className="cell"></div>
+    <div className="cell"></div>
+    <div className="cell"></div>
+    <div className="cell"></div>
+    <div className="cell"></div>
+    <div className="cell"></div>
+    <div className="cell"></div>
+  </div>
+
+};
+
+
+export default Board;
