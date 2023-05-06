c13388fce6a84e7e6c226e67fcdbee3bfddae788
luiiizz <luizhenriquebispo05@gmail.com>
2023-05-06 18:12:15 -0300

    v1.0.4 Add 2CRUD para tabela aeronaves

--- S9/Aero_Express/database.js
+++ S9/Aero_Express/database.js
@@ -1,8 +1,16 @@
 const { Sequelize} = require('sequelize');
+require('dotenv').config()
 
-const sequelize = new Sequelize('aero', 'root', 'root',{
-    host: 'localhost',
-    dialect: 'mysql',
+const dbName = process.env.DB_NAME; // passar os dados do .env para as constantes
+const dbUser = process.env.DB_USER;
+const dbHost = process.env.DB_HOST;
+const dbPassword = process.env.DB_PASSWORD;
+
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    //passar os dados para o sequelize
+    host: dbHost, //o host, neste caso estamos com um banco local
+    dialect: "mysql", //informar o tipo de banco que vamos utilizar
 });
 
 module.exports = sequelize
